febf350b02f2eacff1f1acd747860868b0eaa84e
jinijini20 <jinijini301301@gamil.com>
2025-12-11 12:05:51 +0900

    Feat: 변수사용,문의사항 만들기,redirect()

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,redirect,request
 
 app=Flask(__name__)
 
@@ -6,6 +6,22 @@ app=Flask(__name__)
 def index():
     return "Hello FlaskBook!"
 
-@app.route("/hello/<name>",methods=["GET"],endpoint="hello-endpoint")
-def hello(name):
-    return f"Hello {name} World!"
+@app.route("/name/<int:age>")
+def show_name(age):
+    return render_template("index_old.html",age=age)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete",methods=["GET","POST"])
+def contact_complete():
+    if request.method == "POST":
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
+
+## url확인용이다.
+with app.test_request_context():
+    print(url_for("index_old"))
+    print(url_for("show_name",name='AK'))
--- flaskbook2/apps2/minimal2/app.py
+++ flaskbook2/apps2/minimal2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,redirect,requests
 
 app=Flask(__name__)
 
@@ -10,11 +10,26 @@ def index():
 def jenny():
     return "hello black pink!"
 
-@app.route("/hello/<name>",methods=["GET"],endpoint="hello-endpoint")
-def hello_jenny(name):
-    return f"hello {name}"
+@app.route("/hello/<int:age>")
+def hello_jenny(age):
+    return render_template("index.html",age=age)
 
 @app.route("/jennyage/<int:age>",methods=["GET"])
 def jennyage(age):
     return f"old {age}"
 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete",methods=["GET","POST"])
+def contact_complete():
+    if requests.method=="POST":
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
+
+## url확인용 이다.
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("hello_jenny",age="12"))
