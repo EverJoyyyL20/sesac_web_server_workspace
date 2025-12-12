8980cade85545b2cb1782e14ab3925ec9c1f12c0
jinijini20 <jinijini301301@gamil.com>
2025-12-12 16:23:22 +0900

    Feat: 이메일 송신 구축

--- flaskbook/app.py
+++ flaskbook/app.py
@@ -1,9 +1,25 @@
-# flask 클래스를 import 한다                 
-from flask import Flask
+from email_validator import EmailNotValidError, validate_email
+from flask import (Flask, 
+                   current_app, 
+                   make_response, 
+                   redirect, 
+                   render_template, 
+                   request, 
+                   session, 
+                   url_for, 
+                   flash)
 
+import os
+from flask_mail import Mail,Message
 # flask 클래스를 인스턴스화한다
 app = Flask(__name__)
 
+
+app.config["SECREY_KEY"]=os.urandom(24)
+app.config["MAIL_SERVER"]=os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"]=os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"]=os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"]
 # URL과 실행할 함수를 매핑한다
 @app.route("/")
 def index():
